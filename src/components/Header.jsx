6c649baf1e63377e706939dedd0506323b30eff5
Volodymyr Chernenko <vchernenko@horizonsoftware.com>
2019-04-15 16:05:12 +0300

    react project

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src='http://configuratormedia.lamborghini.com/renderservice/media/fast/H4sIAAAAAAAAAI1QMW_TQBj94hJoClRQIXUCMbBiBC0SiMkJFjU4NDjOkHQwl-TDdbHvzOdzCAwRMNCBFTYGBiaI-CFISEioYq1YUVkYQEgonKtcCxs3PJ3uvXvf9954B8o5weKau8EGzIyE2USKWBw9ZN0Yr7z7WBk3P-84BsAwBYATI4LLPZGYPcFjFJlJyPtIGdIg6qH6HjP1qASJ4FOnFYxTpDP-gxRhehaUG8H87sSY8dC0eZ78TaYSZq837GuB61QllGVOXTG6ByMwJBzZJW61LNfx2xnB8X0bh0sMkRa-vHr94_HmJQNKDpQHLM5RjTu2r7uZJ12kp-MXJw8_336ms3X0hErdqduB327YEuaihIV4biPFUNNzVat2I6ituqueas5Yq46_L_48OOtva6OZyWSixUcbTs1veXZgeZ7VzorXipKUNH9oyqsgS__RK-fYk4LMFZat10RfN1oyoOyAcf98ARcKWCpguYCLw3TLu3r3zlfafPL71Prt1tv3wzfwSa3GBsgl6ws6i0OpFzrQdDq23nNmWd-M8N-qq0LEyPiH0_Ro6-Wvb6rqjq46hb2YsFdpy3ODhuVZdQnzOcUpI6bCkkj5H7toOISCAgAA.webp?wid=1050' />
+      </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src='https://signoftherose.files.wordpress.com/2016/08/sea-05.jpg' />
+        </div>
+        <div>
+          avatar + description
+        </div>
+        <div>
+          My post
+        <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 1
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    )
+}
+export default Profile
